d0086849c92c2ae1b2364bab48ab73fb1e71ee92
Raretie <rareworksheesh@gmail.com>
2026-05-19 11:24:01 +0700

    Adding something missing

--- penjaga/urls.py
+++ penjaga/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('api/scan-rfid/', views.scan_rfid, name='scan_rfid'),
-    path('api/start-sesi/', views.start_sesi, name='start_sesi'),
+    path('api /start-sesi/', views.start_sesi, name='start_sesi'),
     path('api/scan-tong/', views.scan_tong, name='scan_tong'),
     path('api/submit-sesi/', views.submit_sesi, name='submit_sesi'),
 ]
